f410f84f84722c1131c557ece0bd34011ded6114
Steve Tentrop <steve.tentrop@plentymarkets.com>
2022-05-25 23:13:30 +0200

    Update CashInAdvanceController.php

--- src/Controllers/CashInAdvanceController.php
+++ src/Controllers/CashInAdvanceController.php
@@ -2,6 +2,8 @@
 
 namespace CashInAdvance\Controllers;
 
+use Plenty\Modules\Webshop\Contracts\ContactRepositoryContract;
+use Plenty\Modules\Webshop\Contracts\SessionStorageRepositoryContract;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
 
@@ -9,10 +11,24 @@ class CashInAdvanceController extends Controller
 {
 	public function getBankDetails(Twig $twig, $lang)
 	{
-	    if ($lang === null || !strlen($lang)) {
-	        $lang = 'de';
-        }
-	    
+		if ($lang === null || !strlen($lang)) {
+			$lang = 'de';
+		}
+
+		/** @var ContactRepositoryContract $contactRepository */
+		$contactRepository = pluginApp(ContactRepositoryContract::class);
+
+		$contactId = $contactRepository->getContactId();
+
+		if ($contactId == 0) {
+		    /** @var SessionStorageRepositoryContract $sessionStorage */
+		    $sessionStorage = pluginApp(SessionStorageRepositoryContract::class);
+		    $lastAccessOrderId = $sessionStorage->getSessionValue(SessionStorageRepositoryContract::LAST_ACCESSED_ORDER);
+
+		    if (is_null($lastAccessOrderId)) {
+			return 'Unauthorized';
+		    }
+		}
 		return $twig->render('PrePayment::BankDetailsOverlay', ['lang' => $lang]);
 	}
 }
